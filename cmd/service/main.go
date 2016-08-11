492f5fb57fa9eeee32f3377f382e68d42f240837
cheyang <cheyang@163.com>
2016-08-11 15:52:27 +0800

    grpc service

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"flag"
+	"net"
+	"os"
+	"os/signal"
+
+	log "github.com/Sirupsen/logrus"
+	numa "github.com/cheyang/numa-utils/proto"
+	"github.com/cheyang/numa-utils/service"
+)
+
+var bind string
+
+func main() {
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	interrupts := make(chan os.Signal)
+	signal.Notify(interrupts, os.Interrupt)
+	go func() {
+		<-interrupts
+		cancel()
+		os.Exit(0)
+	}()
+
+	listen, err := net.Listen("tcp", bind)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	server := service.Server{}
+	s := grpc.NewServer()
+	numa.RegisterNumaServer(s, server)
+
+	panic(s.Serve(listen))
+
+}
+
+func init() {
+	flag.StringVar(&bind, "bind", ":20000", "Service bind address")
+}
